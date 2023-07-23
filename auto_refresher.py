7028333ed629f8ee0b19b1c3d3ffe184eb7df4ae
Mehul Verma <mehulverma04@gmail.com>
2023-07-23 21:17:01 +0530

    minor changes

--- auto_refresher.py
+++ auto_refresher.py
@@ -1,6 +1,7 @@
 import pyautogui
 import time
+
 time.sleep(2)
 while True:
-    pyautogui.PAUSE=10
-    pyautogui.hotkey("ctrl","r")
+    pyautogui.PAUSE = 10
+    pyautogui.hotkey("ctrl", "r")
--- auto_typer.py
+++ auto_typer.py
@@ -1,12 +1,16 @@
 import pyautogui
 import time
+
+
 def hit(key):
     pyautogui.keyDown(key)
     return
-word=input("Enter the text to auto type: ")
+
+
+word = input("Enter the text to auto type: ")
 time.sleep(2)
 while True:
-    pyautogui.PAUSE=0.1
+    pyautogui.PAUSE = 0.1
     # hit("w")
     pyautogui.typewrite(word)
     hit("Enter")
